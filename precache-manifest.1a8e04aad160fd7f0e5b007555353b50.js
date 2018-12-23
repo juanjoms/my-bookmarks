49fbe581b74d867ac58c1375717103e6f2b03b4f
Juan Miramontes <juan.miramontes@accenture.com>
2018-12-23 01:24:03 -0600

    Updates

---
+++ precache-manifest.1a8e04aad160fd7f0e5b007555353b50.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "277aee373614b5bde2ec5d0c2c63d113",
+    "url": "/home/static/media/self-link_light.277aee37.svg"
+  },
+  {
+    "revision": "d7e962e2670f9467929b36a910821b9c",
+    "url": "/home/static/media/self-link_dark.d7e962e2.svg"
+  },
+  {
+    "revision": "37f137de267880be27a52acd5e64ef8c",
+    "url": "/home/static/media/external-link_light.37f137de.svg"
+  },
+  {
+    "revision": "24126654a0fbcfaaefa952e15a22bca5",
+    "url": "/home/static/media/external-link_dark.24126654.svg"
+  },
+  {
+    "revision": "e258ea1747dec9816919",
+    "url": "/home/static/js/runtime~main.e258ea17.js"
+  },
+  {
+    "revision": "ccaf4008554be28f275b",
+    "url": "/home/static/js/main.ccaf4008.chunk.js"
+  },
+  {
+    "revision": "e40103a0a0b7a8b908de",
+    "url": "/home/static/js/1.e40103a0.chunk.js"
+  },
+  {
+    "revision": "ccaf4008554be28f275b",
+    "url": "/home/static/css/main.d89708a2.chunk.css"
+  },
+  {
+    "revision": "8e5186105555d07a6a74ad1351deff72",
+    "url": "/home/index.html"
+  }
+];
